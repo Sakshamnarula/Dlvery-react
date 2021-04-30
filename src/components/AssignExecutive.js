b2df4335883064c766138ef00c1927280b47b211
steve <deep.gagan404@gmail.com>
2021-04-30 14:10:42 +0530

    Assign Executive

--- src/components/AssignExecutive.js
+++ src/components/AssignExecutive.js
@@ -1,1 +1,49 @@
 //placeholder
+
+import React from 'react';
+import react from 'react';
+import bootstrap from 'react-bootstrap'
+
+class AssignExecutive extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            executives: [],
+            inventories: [],
+
+        }
+
+        this.fetchExecutives = this.fetchExecutives.bind(this)
+        this.fetchInventories = this.fetchInventories.bind(this)
+
+    }
+
+    fetchInventories = () => {
+
+    }
+
+    fetchExecutives = () => {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (<div>
+            <div className="container">
+                <div className="col">
+                    <div className="card-col-md">
+                        <h3 className="text-center"> Assign Executive</h3>
+                        <div className="d-flex">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
+    }
+}
+export default AssignExecutive;
